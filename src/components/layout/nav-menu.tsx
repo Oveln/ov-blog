5ffae9aaa2047a7b909c887d9ee79038b4cb5075
Oveln <oveln@outlook.com>
2024-01-24 07:01:29 +0800

    首页

---
+++ src/components/layout/nav-menu.tsx
@@ -0,0 +1,41 @@
+"use client";
+import * as React from "react";
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle
+} from "../ui/navigation-menu";
+import Link from "next/link";
+export function NavMenu() {
+    return (
+        <NavigationMenu className="my-2 sticky top-2">
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <Link href="/" className={navigationMenuTriggerStyle()}>
+                        Home
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>Blogs</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        {/* <NavigationMenuLink className="p-96">Link</NavigationMenuLink> */}
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>Apps</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        {/* <NavigationMenuLink>Link</NavigationMenuLink> */}
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link className={navigationMenuTriggerStyle()} href="/about" passHref>
+                        About
+                    </Link>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    );
+}
