772fff3017e3c841a8f72050f811477412b612b8
Oveln <oveln@outlook.com>
2024-01-25 10:33:04 +0800

    修复首页按钮在手机显示重叠

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-'use client'
+"use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
 import useLoaded from "@/hooks/useLoaded";
 import { EnvelopeClosedIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
 import clsx from "clsx";
@@ -18,8 +17,7 @@ const ovLinks = [
         url: "mailto:oveln@outlook.com",
         icon: <EnvelopeClosedIcon className="size-5" />
     }
-
-]
+];
 
 export default function Home() {
     const isLoaded = useLoaded();
@@ -27,29 +25,35 @@ export default function Home() {
         <main className={clsx("relative h-[calc(100vh-56px)]", isLoaded && "fade-in-start")}>
             <div className="absolute mt-[15%] flex w-full items-center md:flex-row flex-col">
                 <div className="flex-1 p-10 md:pl-0">
-                    <h1 className="text-2xl md:text-4xl 2xl:text-5xl" data-fade='1'>{"Hi!👋"}</h1>
-                    <h1 className="mt-1 text-3xl md:text-5xl 2xl:text-6xl" data-fade='2'>
+                    <h1 className="text-2xl md:text-4xl 2xl:text-5xl" data-fade="1">
+                        {"Hi!👋"}
+                    </h1>
+                    <h1 className="mt-1 text-3xl md:text-5xl 2xl:text-6xl" data-fade="2">
                         {"I'm "}
                         <span className="text-fuchsia-500 text-opacity-85">Oveln🎉</span>
                     </h1>
-                    <h1 className="mt-5 text-lg" data-fade='3'>A Student && Developer</h1>
-                    <h1 className="text-lg" data-fade='3'>Love Coding⌨️ and Gameing🎮</h1>
-                    <div className="mt-2 grid grid-cols-5" data-fade='4'>
-                        {
-                            ovLinks.map((link) => {
-                                return (
-                                    <Button variant="link" className="p-0 mr-auto" asChild key={link.name}>
-                                        <Link href={link.url}>
-                                            {link.icon}
-                                            <span className="ml-1">{link.name}</span>
-                                        </Link>
-                                    </Button>
-                                );
-                            })
-                        }
+                    <h1 className="mt-5 text-lg" data-fade="3">
+                        A Student && Developer
+                    </h1>
+                    <h1 className="text-lg" data-fade="3">
+                        Love Coding⌨️ and Gameing🎮
+                    </h1>
+                    <div className="mt-2" data-fade="4">
+                        {ovLinks.map((link) => {
+                            return (
+                                <Link
+                                    key={link.name}
+                                    href={link.url}
+                                    className="p-0 mr-1 inline-flex w-fit animated-underline hover:animated-underline-hover"
+                                >
+                                    {link.icon}
+                                    <span className="ml-1">{link.name}</span>
+                                </Link>
+                            );
+                        })}
                     </div>
                 </div>
-                <div className="flex-1" data-fade='1'>
+                <div className="flex-1" data-fade="1">
                     <Avatar className="size-72 mx-auto">
                         <AvatarImage src="./avatar.jpg" />
                         <AvatarFallback>Oveln</AvatarFallback>
