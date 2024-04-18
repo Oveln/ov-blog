43baf37be30ac19e404f06e242a96830048b8cf1
Oveln <oveln@outlook.com>
2024-04-18 16:17:05 +0800

    增加文章页，增加markdown渲染

--- src/data/db.ts
+++ src/data/db.ts
@@ -1,19 +1,29 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-export const getAllPostCardInfo = async ()=> { return await prisma.post.findMany(
-    // published
-    // 只需要文章标题和描述
-    {
-        select: {
-            id: true,
-            title: true,
-            description: true,
-            create_time: true
-        },
-        where: {
-            // published: true
+export const getAllPostCardInfo = async () => {
+    return await prisma.post.findMany(
+        // published
+        // 只需要文章标题和描述
+        {
+            select: {
+                id: true,
+                title: true,
+                description: true,
+                create_time: true
+            },
+            where: {
+                // published: true
+            }
         }
-    }
-)}
+    );
+};
+
+export const getPostById = async (id: number) => {
+    return await prisma.post.findUnique({
+        where: {
+            id: id
+        }
+    });
+};
