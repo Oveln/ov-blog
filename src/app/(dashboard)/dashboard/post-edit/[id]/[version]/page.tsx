9c25ea9e0c39198fa706fcb5f75fd157a11be2b2
Oveln <oveln@outlook.com>
2024-04-22 21:46:28 +0800

    文章提交以及编辑页面修改

--- src/app/(dashboard)/dashboard/post-edit/[id]/[version]/page.tsx
+++ src/app/(dashboard)/dashboard/post-edit/[id]/[version]/page.tsx
@@ -3,74 +3,145 @@ import MDEditor from "@uiw/react-md-editor";
 import React, { useState, useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { notFound } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { Loading } from "@/components/ui/loading";
+import { GetPostVersionType } from "@/app/(auth)/api/post/[id]/[version]/route";
+import { NewPostVersionRetType, NewPostVersionType } from "@/app/(auth)/api/post/[id]/route";
+import { toast } from "sonner";
+import { RotateCcw, Send } from "lucide-react";
 
 type ERROR = {
     error: string;
 };
 
-type Post_Version = {
-    id: number;
-    title: string;
-    description: string | null;
-    content: string;
-    version: number;
-    create_time: Date;
-    update_time: Date;
-    postId: number;
-} | null;
-
-enum PageState {
-    LOADING,
-    ERROR,
-    LOADED
-}
-
 export default function PostEdit({ params }: { params: { id: string; version: string } }) {
-    const [pageState, setPageState] = useState<PageState>(PageState.LOADING);
+    const [title, setTitle] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
     const [content, setContent] = useState<string>("");
-    let postVersion: Post_Version = null;
+    const [postVersion, setPostVersion] = useState<GetPostVersionType>(null);
+
     useEffect(() => {
         const loadData = async () => {
             try {
                 const res = await fetch(`/api/post/${params.id}/${params.version}`);
                 const data = await res.json();
                 console.log(data);
-                postVersion = data;
-                if (!postVersion) {
-                    setPageState(PageState.ERROR);
-                    return;
+                if (!data) {
+                    useRouter().push("404");
                 }
-                setContent(postVersion?.content);
-                setPageState(PageState.LOADED);
+                setPostVersion(data as GetPostVersionType);
+                setTitle(data.title);
+                setDescription(data.description || "");
+                setContent(data.content);
             } catch {
-                setPageState(PageState.ERROR);
+                useRouter().push("404");
             }
         };
         loadData();
     }, []);
+
+    if (!postVersion) {
+        return <Loading />;
+    }
+
+    const submit = async () => {
+        if (!title) {
+            toast("不可以是空标题！");
+            return;
+        }
+        const postData: NewPostVersionType = {
+            title: title,
+            description: description == "" ? null : description,
+            content: content,
+            postId: postVersion.postId,
+            published: true
+        };
+        const res: NewPostVersionRetType = await (
+            await fetch(`/api/post/${postVersion.postId}`, {
+                method: "POST",
+                body: JSON.stringify(postData),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+        ).json();
+        switch (res.status) {
+            case "ok":
+                toast("提交成功", {
+                    description: "提交成功",
+                    action: {
+                        label: "查看",
+                        onClick: () => {
+                            useRouter().push(`/dashboard/post/${postVersion.postId}`);
+                        }
+                    }
+                });
+                return;
+            case "error":
+                toast("提交失败", {
+                    description: "请检查网络连接"
+                });
+                return;
+            case "busy":
+                toast("提交失败", {
+                    description: "请勿频繁提交"
+                });
+                return;
+            case "unauthorized":
+                toast("提交失败", {
+                    description: "请先登录"
+                });
+                return;
+        }
+    };
+
     const handleChange = (value: string | undefined) => {
         setContent(value || "");
     };
 
-    if (pageState == PageState.LOADING) {
-        return <Loading />;
-    }
-
-    if (pageState == PageState.ERROR) {
-        return notFound();
-    }
-
     return (
         <div className="flex flex-col relative h-full">
-            <div className="flex w-1/4 my-2">
-                <span className="flex items-center justify-center text-xl px-2">标题</span>
-                <Input className="flex-1 w-"></Input>
-            </div>
-            <div className="flex w-1/4 my-2">
-                <span className="flex items-center justify-center text-xl px-2">描述</span>
-                <Input className="flex-1 w-"></Input>
+            <div className="flex">
+                <div className="flex flex-col flex-1 py-2">
+                    <div className="flex w-3/5 mb-2 flex-1">
+                        <span className="flex items-center justify-center text-xl px-2">标题</span>
+                        <Input
+                            value={title}
+                            onChange={(s) => setTitle(s.target.value)}
+                            placeholder="请输入标题"
+                            className="flex-1"
+                        ></Input>
+                    </div>
+                    <div className="flex w-3/5 flex-1">
+                        <span className="flex items-center justify-center text-xl px-2">简介</span>
+                        <Input
+                            value={description}
+                            onChange={(s) => setDescription(s.target.value)}
+                            placeholder=""
+                            className="flex-1 w-"
+                        ></Input>
+                    </div>
+                </div>
+                <div className="flex flex-col flex-1 py-2">
+                    <Button className="ml-auto mr-2 mb-2 flex-1" variant="outline" onClick={submit}>
+                        <Send className="h-5 w-5 mr-2 " />
+                        <span>提交</span>
+                    </Button>
+
+                    <Button
+                        className="ml-auto mr-2 flex-1 mb-2"
+                        variant="outline"
+                        onClick={() => {
+                            toast("重置!", {
+                                description: "重置成功"
+                            });
+                            setContent(postVersion.content);
+                        }}
+                    >
+                        <RotateCcw className="h-5 w-5 mr-2 " />
+                        <span>重置</span>
+                    </Button>
+                </div>
             </div>
             <div className="flex-1 p-2">
                 <MDEditor
@@ -80,17 +151,6 @@ export default function PostEdit({ params }: { params: { id: string; version: st
                     onChange={handleChange}
                 />
             </div>
-            <div className="flex mx-auto">
-                <Button className="w-24 h-12 my-auto m-2" variant="outline" onClick={() => {}}>
-                    <span>重置</span>
-                </Button>
-                <Button className="w-24 h-12 my-auto m-2" variant="outline" onClick={() => {}}>
-                    <span>提交</span>
-                </Button>
-            </div>
         </div>
     );
-}
-function loading() {
-    return <div>Loading...</div>;
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { clearTimeout } from "timers";
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
+}
+
+export function debounce(func: () => void, wait: number) {
+    let timeout: NodeJS.Timeout | null = null;
+    if (timeout) {
+        clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+        func();
+    }, wait);
 }
