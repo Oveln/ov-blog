7d4b15a88f5b1c169dc1ee4eca6c3ebd5a14b4ec
Oveln <oveln@outlook.com>
2024-04-24 21:43:45 +0800

    依赖更新，无用依赖丢弃

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 /** @type {function(import("next").NextConfig): import("next").NextConfig}} */
-import removeImports from "next-remove-imports";
-const removeImportsFun = removeImports({
-    // test: /node_modules([\s\S]*?)\.(tsx|ts|js|mjs|jsx)$/,
-    // matchImports: "\\.(less|css|scss|sass|styl)$"
-});
 const nextConfig = {
     images: {
         remotePatterns: [
