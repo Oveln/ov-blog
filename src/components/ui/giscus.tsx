ee1e7762766b240033a2f7c0471b5cecbde1d580
Oveln <oveln@outlook.com>
2024-11-21 09:58:38 +0800

    使评论区由环境变量配置

--- src/components/ui/giscus.tsx
+++ src/components/ui/giscus.tsx
@@ -3,13 +3,20 @@ import * as React from "react";
 import Giscus from "@giscus/react";
 
 export default function CommentsArea() {
+    const repo = process.env.NEXT_PUBLIC_REPO;
+    const repoId = process.env.NEXT_PUBLIC_REPOID;
+    const category = process.env.NEXT_PUBLIC_CATEGORY;
+    const categoryId = process.env.NEXT_PUBLIC_CATEGORYID;
+    if (!repo || !repoId || !category || !categoryId) {
+        return <></>;
+    }
     return (
         <Giscus
             id="comments"
-            repo="Oveln/ov-blog"
-            repoId="R_kgDOLIzmXw"
-            category="Announcements"
-            categoryId="DIC_kwDOLIzmX84CgJLj"
+            repo={repo as `${string}/${string}`}
+            repoId={repoId}
+            category={category}
+            categoryId={categoryId}
             mapping="pathname"
             term="Welcome to Oveln Blog component!"
             reactionsEnabled="1"
