0356f3fa8a9ac324fd943e65b3a095f109299bab
Oveln <oveln@outlook.com>
2024-11-27 02:23:06 +0800

    美化登录界面

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -14,10 +14,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <html lang="en" className="m-0 p-0 bg-muted/40" suppressHydrationWarning>
+        <html lang="en" className="m-0 p-0" suppressHydrationWarning>
             <body
                 className={
-                    "mx-auto max-w-[68rem] px-2 lg:px-0 w-screen h-screen bg-muted/40 " +
+                    "mx-auto max-w-[68rem] px-2 lg:px-0 w-screen h-screen " +
                     inter.className
                 }
             >
